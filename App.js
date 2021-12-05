86f6748b625748ebc1ee5a15258410b6bea5eae2
Tomasrieck <Tomasrieck@gmail.com>
2021-12-06 00:13:30 +0100

    App

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import Settings from './app/screens/Settings';
+
+
+const Stack = createNativeStackNavigator();
+
+
+const MyStack = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={WelcomeScreen} />
+        <Stack.Screen name="Settings" component={Settings} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default MyStack
---
+++ app/screens/Settings.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, Button, View, Image, SafeAreaView, TouchableWithoutFeedback, useColorScheme } from 'react-native';
+
+
+
+const Settings = ({ navigation }) => {
+    const colorScheme = useColorScheme();
+
+    // const themeTextStyle = colorScheme === 'light' ? styles.lightThemeText : styles.darkThemeText;
+    const themeContainerStyle = colorScheme === 'light' ? styles.lightTheme : styles.darkTheme;
+    return (
+        <View style={[styles.background, themeContainerStyle]}>
+            <SafeAreaView style={[styles.bar, {top: 0}]}>
+                <Image style={[styles.icons, themeContainerStyle]} source={require("../assets/homeIcon.png")} />
+                <Image style={[styles.icons, themeContainerStyle]} source={require("../assets/homeIcon.png")} />
+                <Image style={[styles.icons, themeContainerStyle]} source={require("../assets/settingsIcon.png")} />
+            </SafeAreaView>
+            <View style={styles.content}>
+
+            </View>
+            <SafeAreaView style={[styles.bar, {bottom: 0}]}>
+                <Image style={[styles.icons, themeContainerStyle]} source={require("../assets/homeIcon.png")} />
+                <TouchableWithoutFeedback onPress = {() => navigation.navigate('Home')} >
+                    <Image style={[styles.icons, themeContainerStyle]} source={require("../assets/homeIcon.png")} />
+                </TouchableWithoutFeedback>
+                <Image style={[styles.icons, themeContainerStyle]} source={require("../assets/menuIcon.png")} />
+            </SafeAreaView>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+    },
+    lightTheme: {
+        backgroundColor: 'white',
+        tintColor: 'black',
+    },
+    darkTheme: {
+        backgroundColor: 'black',
+        tintColor: 'white',
+    },
+    bar: {
+        flex: 0.1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        width: '100%',
+    },
+    icons: {
+        width: 35,
+        height: 35,
+        marginTop: 15,
+        marginBottom: 15,
+    },
+    content: {
+        backgroundColor: 'dodgerblue',
+        flex: 1,
+    }
+})
+
+
+export default Settings;
